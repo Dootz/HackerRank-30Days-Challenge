952a6498acd20c1c6c976ee7592b6b001355667a
Dootz <k.kozubek@wp.pl>
2017-04-10 11:38:22 +0200

    Day 4

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -11,6 +11,10 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
+            Person human = new Person(12);
+            Person human1 = new Person(-1);
+            Person human2 = new Person(15);
+            Person human4 = new Person(22);
         }
     }
 }
---
+++ HackerRank/Solutions/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Solutions
+{
+    // Challenge Day 4
+    class Person
+    {
+        public int age;
+        public Person(int initialAge)
+        {
+            // Add some more code to run some checks on initialAge
+            if (initialAge < 0)
+            {
+                age = 0;
+                Console.WriteLine("Age is not valid, setting age to 0.");
+            }
+            else
+            {
+                age = initialAge;
+                amIOld();
+            }
+        }
+        public void amIOld()
+        {
+            string ageString;
+            // Do some computations in here and print out the correct statement to the console 
+            if (age < 13)
+                ageString = "You are young.";
+            else if (age >= 13 && age < 18)
+                ageString = "You are a teenager";
+            else
+                ageString = "You are old.";
+            Console.WriteLine(ageString);
+        }
+
+        public void yearPasses()
+        {
+            // Increment the age of the person in here
+            age++;
+        }
+    }
+}
