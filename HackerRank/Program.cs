0d5edc86814421246f35df5777b157cf866b5f3c
Dootz <k.kozubek@wp.pl>
2017-04-11 09:28:52 +0200

    Day 5

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -11,10 +11,7 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            Person human = new Person(12);
-            Person human1 = new Person(-1);
-            Person human2 = new Person(15);
-            Person human4 = new Person(22);
+            HackerRank.Solutions.Solutions.Day4();
         }
     }
 }
--- HackerRank/Solutions/Solutions.cs
+++ HackerRank/Solutions/Solutions.cs
@@ -56,5 +56,13 @@ namespace HackerRank.Solutions
 
             Console.WriteLine(answer);
         }
+
+        public static void Day4()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine(n + " x " + i + " = " + n * i);
+        }
     }
 }
