99b39c3e0e09fca2274b335811ac656a9c27f89d
Dootz <k.kozubek@wp.pl>
2017-04-29 19:01:56 +0200

    Day 21 - Generics

---
+++ HackerRank/Solutions/Printer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Solutions
+{
+    class Printer
+    {
+        public static void PrintArray<T>(T[] inputArray)
+        {
+            foreach(T element in inputArray)
+            {
+                Console.WriteLine(element.ToString());
+            }
+        }
+    }
+}
--- HackerRank/Solutions/Solutions.cs
+++ HackerRank/Solutions/Solutions.cs
@@ -361,6 +361,26 @@ namespace HackerRank.Solutions
             Console.WriteLine("Last Element: " + a[n - 1]);
         }
 
+        public static void Day21()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] stringArray = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                stringArray[i] = Console.ReadLine();
+            }
+
+            Printer.PrintArray<Int32>(intArray);
+            Printer.PrintArray<String>(stringArray);
+        }
+
         public static void display(Node head)
         {
             Node start = head;
